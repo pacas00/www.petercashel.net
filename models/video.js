981b2c904616be64a0c0a45ae9eb0aef08cba21a
Peter Cashel <pacas00@gmail.com>
2015-06-26 20:01:24 +1000

    update video model

--- models/video.js
+++ models/video.js
@@ -2,14 +2,30 @@ var keystone = require('keystone');
 var Types = keystone.Field.Types;
 
 var Videos = new keystone.List('Videos', {
-	map: { name: 'videoTitle' },
-	autokey: { path: 'slug', from: 'videoTitle', unique: true }
+	map: { name: 'videoKey' },
+	autokey: { path: 'slug', from: 'videoKey', unique: true }
 });
 
 Videos.add({
-    videoTitle: { type: String, required: true },
-	videoUrl: { type: Types.Url },
+    videoKey: { type: String, required: true },
+	videoUrl: { type: Types.Url, required: true },
     videoData: { type: Types.Embedly, from: 'videoUrl' }
+});
+
+Videos.schema.virtual('videoHTML').get(function() {
+	return this.videoData.html;
+});
+
+Videos.schema.virtual('videoTitle').get(function() {
+	return this.videoData.title;
+});
+
+Videos.schema.virtual('videoDescription').get(function() {
+	return this.videoData.description;
+});
+
+Videos.schema.virtual('videoAuthorUrl').get(function() {
+	return this.videoData.authorUrl;
 });
 
 Videos.register();
