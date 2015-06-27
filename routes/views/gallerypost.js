2e33d57d83fffdc694b2cbfec59d118caa34033b
Peter Cashel <pacas00@gmail.com>
2015-06-27 14:00:46 +1000

    this?

--- routes/views/gallerypost.js
+++ routes/views/gallerypost.js
@@ -8,18 +8,22 @@ exports = module.exports = function(req, res) {
 	// Set locals
 	locals.section = 'gallery';
 	locals.filters = {
-		id: req.params.id
+		gallerypost: req.params.gallerypost
+	};
+	//Does this need to be here to work?
+	locals.data = {
+		galleryposts: []
 	};
 	
 	// Load the current post
 	view.on('init', function(next) {
 		
 		var q = keystone.list('Gallery').model.findOne({
-			key: locals.filters.id
-		});
+			key: locals.filters.gallerypost
+		}).populate();
 		
 		q.exec(function(err, result) {
-			locals.data.id = result;
+			locals.data.gallerypost = result;
 			next(err);
 		});
 		
